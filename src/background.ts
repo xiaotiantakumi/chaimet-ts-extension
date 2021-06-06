d4e48d7ab9a959e280dc1139c2ed998e65144b0d
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-06 17:56:51 +0900

    javascriptからtypescriptに書き換え中

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,50 @@
-import moment from 'moment';
+// const moment = require('moment');
 import axios from 'axios';
+import {WordInfo} from './chaimet'
 
-const now = moment();
-(async () => {
-  const res = await axios.get('https://www.google.com/search?q=test');
-  console.log({
-    from: 'background.ts',
-    now: now.format('YYYY/MM/DD HH:mm:ss'),
-    data: res.data,
+let fromTabId = -1;
+const baseUrl : string = "https://cjjc.weblio.jp/content/";
+chrome.runtime.onMessage.addListener((request,sender,sendMessage)=>{
+  fromTabId = sender?.tab?.id ?? -1;
+  if(fromTabId == -1) return;
+
+  if(request.type==='selectedMsg'){
+  let word = encodeURI(request.msg);
+  axios.get(baseUrl + word)
+  .then((res) => {
+    let doc = new DOMParser().parseFromString(res.data, "text/html");
+    let titleEle = doc.getElementsByClassName('pnyn') as HTMLCollectionOf<HTMLElement>;
+    let descriptionEle = doc.getElementsByClassName('level0') as HTMLCollectionOf<HTMLElement>;
+    let wordInfo = new WordInfo('',titleEle,descriptionEle);
+    chrome.tabs.sendMessage(fromTabId, {
+        type: 'getWordInfo',
+        msg:wordInfo
+    });
+
+    // すべてのタブを取得して何かしらの操作をする
+    chrome.tabs.query({},afterGetAllTabs);
   })
-})();
+  .catch((error) => {
+    console.log('ERROR!! occurred in Backend.')
+  });
+  }
+})
+
+const urlPtn = '';
+function afterGetAllTabs(tabs : chrome.tabs.Tab[]){
+  let hasTab = false;
+  tabs.forEach((tab) => {
+    if (urlPtn == tab.url) {
+      hasTab = true;
+      // ここで該当するタブをアクティブにする。タブIDが必要となる。
+      
+      chrome.tabs.update(tab.id ?? -1, { selected : true});
+      // chrome.tabs.sendMessage(tab.id ?? -1, wordInfo);
+    } else{
+      tab.active = false;
+    }
+  });
+  // タブが見つからなかった時の処理
+  // if(hasTab == false && formTabId != -1){
+  // }
+}
--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,81 @@
-import moment from 'moment';
-import axios from 'axios';
-import {SampleClass} from './sampleClass';
+import { WordInfo } from "./chaimet";
 
-const now = moment();
-(async () => {
-  var cls = new SampleClass("TACOS");
-  console.log(cls.GetValue());
-  const res = await axios.get('https://www.google.com/search?q=test');
-  console.log({
-    from: 'content.ts',
-    now: now.format('YYYY/MM/DD HH:mm:ss'),
-    data: res.data,
+
+let lastSelectedStr : string;
+let selection : Selection | null = null;
+let doc = window.document;
+let markerTextChar = "\ufeff";
+let tooltipEl : HTMLDivElement;
+let markerEl, markerId = "sel_" + new Date().getTime() + "_" + Math.random().toString().substr(2);
+
+let func = () =>{
+  selection = window.getSelection();
+  let currentStr = selection?.toString();
+  if (!currentStr) {
+    if(tooltipEl) tooltipEl.style.visibility = 'hidden';
+    return;
+  }
+  if (currentStr === lastSelectedStr) {
+    return;
+  }
+  lastSelectedStr = currentStr;
+  // send current selected text 
+  chrome.runtime.sendMessage({
+    type: 'selectedMsg',
+    msg: lastSelectedStr
   })
-})();
+  console.log(selection?.toString());
+}
+setInterval(func, 500);
+
+chrome.runtime.onMessage.addListener((request, sender, sendMessage) => {
+  if (request.type === 'getWordInfo') {
+    // let wordInfo = Object.assign(new WordInfo(), request.msg);
+    let wordInfo : WordInfo = request.msg as WordInfo;
+    if(selection == null) return
+    let range = selection.getRangeAt(0).cloneRange();
+    range.collapse(false);
+    // Create the marker element containing a single invisible character using DOM methods and insert it
+    markerEl = doc.createElement("span");
+    markerEl.id = markerId;
+    markerEl.appendChild(doc.createTextNode(markerTextChar));
+    range.insertNode(markerEl);
+    if (markerEl) {
+      // Lazily create element to be placed next to the selection
+      if (!tooltipEl) {
+        CreateTooltipElement();
+      }
+      if (!wordInfo.pinyin) {
+        return;
+      }
+      tooltipEl.style.visibility = 'visible';
+      let str = '';
+      str += wordInfo.pinyin + '</br>';
+      str +=  wordInfo.description;
+      tooltipEl.innerHTML = str;
+      // Find markerEl position http://www.quirksmode.org/js/findpos.html
+      let obj = markerEl;
+      let left = 0, top = 0;
+      do {
+        left += obj.offsetLeft;
+        top += obj.offsetTop;
+      } while (obj == obj.offsetParent);
+
+      left += 5;
+      top += 5;
+      tooltipEl.style.left = left + "px";
+      tooltipEl.style.top = top + "px";
+
+      markerEl.parentNode?.removeChild(markerEl);
+    }
+  }
+})
+
+function CreateTooltipElement() {
+  tooltipEl = doc.createElement("div");
+  tooltipEl.style.border = "solid darkblue 1px";
+  tooltipEl.style.backgroundColor = "lightgoldenrodyellow";
+  tooltipEl.style.position = "absolute";
+  tooltipEl.style.zIndex = "100";
+  doc.body.appendChild(tooltipEl);
+}
