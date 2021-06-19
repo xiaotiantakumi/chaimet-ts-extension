020832031eeb85bc2e82729246336541d8bd3994
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-19 18:08:43 +0900

    リリース作成のための修正

--- release.js
+++ release.js
@@ -1,8 +1,5 @@
 const zip = require('bestzip');
-let destination = `./release/extension`;
-if (process.argv.length >= 3) {
-  destination += `_${process.argv[2]}`;
-}
+let destination = `./release/chaimet`;
 destination += '.zip';
 zip({
   source: 'dist/*',
